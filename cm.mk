fe30cbdc24afa4dd2fb00f60c20f09a39cf1bca2
Linh Phi <linhphi9x94@gmail.com>
2016-09-25 23:01:02 +0700

    Add unofficial build ID

--- cm.mk
+++ cm.mk
@@ -27,6 +27,9 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Call the proprietary setup
 $(call inherit-product-if-exists, vendor/pantech/ef63l/ef63l-vendor.mk)
 
+ # Unofficial build ID
+TARGET_UNOFFICIAL_BUILD_ID := AA
+
 PRODUCT_NAME := cm_ef63l
 PRODUCT_DEVICE := ef63l
 PRODUCT_MANUFACTURER := PANTECH
